dce84a4a78e31b22cadf95515e92a122b234a331
Da! <86069302+Da4155@users.noreply.github.com>
2024-07-20 23:33:39 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 Work in progress
-
-Installation: 
-
-git clone https://github.com/Da4155/i3_configuration
-
-cd i3_configuration
-
-./install.sh
-
--- installation.sh
+++ installation.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
 #choose between pc and laptop
-echo choose device name
-read -p "'l' for laptop and 'C' for PC" device
+read -p "pc or laptop? p/l" device
+read -p "do you want to use vim? y/n" vim
+read -p "would you like remaped keys? y/n" xmodmap
 
 #installation of required programs for i3
 sudo pacman -S feh
 sudo pacman -S polybar
 sudo pacman -S rofi
-#sudo pacman -S compilor
+sudo pacman -S picom
 sudo pacman -S dunst
 
 
 #i3 configuration files
-mv i3 ~/.config 
+cp -r i3 ~/.config
+echo copied i3 to ~/.config 
 
 #vim config
+cp .vimrc ~
+echo copied .vimrc to ~
 
 #polybar config
+chmod +x polybar/launch.sh
+cp -r polybar ~/.config
+echo copied polybar to ~/.config
 
 #dunst config
+cp -r dunst ~/.config
+echo copied dunst to ~/.config
 
 #libinput
+sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d
+echo copied 30-touchpad.conf to /etc/X11/xorg.conf.d
 
-#if device == l/L
-
-#if device == c/C
+#xmodmap
+cp .Xmodmap ~/.config
+echo copied .Xmodmap tp ~/.config
