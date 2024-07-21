62f225b4cfeab9d3fd46853b1efe73586da8638c
Da! <86069302+Da4155@users.noreply.github.com>
2024-07-22 01:32:53 +0200

    Add files via upload

--- installation.sh
+++ installation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #choose between pc and laptop
-read -p "pc or laptop? p/l" device
-read -p "do you want to use vim? y/n" vim
-read -p "would you like remaped keys? y/n" xmodmap
+read -p "pc(1) or laptop(2)? 1/2" device
+read -p "do you want to use vim (yes=1 no=2)? 1/2" vim
+read -p "would you like remaped keys (yes=1 no=2)? 1/2" xmodmap
 
 #installation of required programs for i3
 sudo pacman -S feh
@@ -22,15 +22,21 @@ mkdir ~/dotfiles
 cp backgrounds ~/dotfiles
 
 #vim config
-if [vim == y]
-then
+if [ $vim == "1" ]; then
 	cp vimrc ~/.vimrc
 	echo copied .vimrc to ~
 fi
 
 #polybar config
-chmod +x polybar/launch.sh
-cp -r polybar ~/.config
+chmod +x lap/polybar/launch.sh
+chmod +x pc/polybar/launch.sh
+
+if [ $device == 2 ]; then   
+	cp -r /lap/polybar ~/.config
+else
+	cp -r pc/polybar ~/.config
+fi
+
 cp -r polybar-scripts ~ 
 echo copied polybar to ~/.config
 
@@ -40,16 +46,14 @@ echo copied dunst to ~/.config
 
 #libinput
 sudo cp 50-my-screensaver.conf /etc/X11/xorg.conf.d
-if [device == l]
-then
+if [ $device == 2 ]; then
 	sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d
 	echo "copied 30-touchpad.conf & 50-my-screensaver.conf to /etc/X11/xorg.conf.d"
 fi
 
 #xmodmap
-if [xmodmap == y]
-then
-	sudo pacman -S xmodmap
+if [ $xmodmap == "1" ]; then
+	sudo pacman -S xorg-xmodmap
 	chmod +x Xmodmap
 	cp Xmodmap ~/.config/.Xmodmap
 	echo copied .Xmodmap tp ~/.config
