ccf436c165fed729396b0ba6b813fb34abfc8009
Da! <86069302+Da4155@users.noreply.github.com>
2024-09-01 02:22:32 +0200

    Add files via upload

--- installation.sh
+++ installation.sh
@@ -37,12 +37,12 @@ fi
 cp rofi ~/.config
 
 #scripts
-chmod +x /scripts/generate_pngs.sh
-chmod +x /scripts/launcher.sh
-chmod +x /scripts/loop_pngs.sh
-chmod +x /scripts/powermenu.sh
-chmod +x /scripts/styles.sh
-chmod +x /scripts/style-switch.sh
+chmod +x scripts/generate_pngs.sh
+chmod +x scripts/launcher.sh
+chmod +x scripts/loop_pngs.sh
+chmod +x scripts/powermenu.sh
+chmod +x scripts/styles.sh
+chmod +x scripts/style-switch.sh
 cp scripts ~/.scripts
 
 #dunst config
